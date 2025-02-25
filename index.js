6c3308ce7ebb61fc3220dee4919ce2e609a52787
byeoon <47872200+byeoon@users.noreply.github.com>
2025-02-25 15:48:01 +0000

    code notes, updated sql command

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  io.emit('chat message', "-> A new user joined the chatroom.", "Server");
+  io.emit('chat message', "-> A new user joined the chatroom.", "Server"); // send connect message as the server (do not put this in the database, its meant to expire)
 
   socket.on('chat message', (msg, username) => {
-    io.emit('chat message', msg, username);
-    var timesent = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    var sendMessagetoDB = `INSERT INTO messages (username, content, timestamp, server) VALUES ('${username}', '${msg}', '${timesent}', 'chat.haydar.dev')`;
+    io.emit('chat message', msg, username); // emit chat message 
+    var timesent = new Date().toISOString().slice(0, 19).replace('T', ' '); // get current time for db
+    var sendMessagetoDB = `INSERT INTO messages (username, content, timestamp, server) VALUES ('${username}', '${msg}', '${timesent}', 'chat.haydar.dev')`; // sql command for db
     con.query(sendMessagetoDB, function (err, result) {
       if (err) throw err;
       console.log("Message added in database.");
@@ -39,14 +39,15 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    io.emit('chat message', "<- A user has left the chatroom.", "Server");
+    io.emit('chat message', "<- A user has left the chatroom.", "Server"); // send disconnect message as the server (do not put this in database, its meant to expire)
   });
 });
 
 server.listen(process.env.PORT, () => {
   console.log(`[Recenter] Now listening on port: ${process.env.PORT}`);
+  // connect to database when we start listening!
   con.connect(function(err) {
     if (err) throw err;
-    console.log("[Recenter] Connected to Recenter database. DB: " + process.env.DATABASE);
+    console.log("[Recenter] Connected to Recenter database. DB: " + process.env.DATABASE); 
   });
 });
